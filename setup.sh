1e52346204ccc6514c60b0dd10a3651339a2eadc
Luka Novak <novak.luka93@gmail.com>
2018-04-15 20:05:41 +0200

    Script for installation and setup

--- setup.sh
+++ setup.sh
@@ -2,3 +2,6 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF50
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
+sudo pip3 install pipenv
+sudo pipenv install
+python -m spacy download en
